f9f5d8f8f7d7edd34543fb5bf81e3cefbb457942
Jeff Cross <middlefloor@gmail.com>
2015-03-02 11:15:47 -0800

    chore: remove debugger statement

--- public/tree.js
+++ public/tree.js
@@ -46,7 +46,6 @@ function baselineDestroyDom() {
   baselineRootTreeComponent.update(new TreeNode('', null, null));
 }
 function baselineCreateDom() {
-  debugger
   var values = count++ % 2 == 0 ? ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '*'] : ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', '-'];
   baselineRootTreeComponent.update(buildTree(maxDepth, values, 0));
 }
